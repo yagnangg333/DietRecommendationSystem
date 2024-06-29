7540a58df2159d5ccfae731cdf4b817f070f392a
yagnangg333 <yagnangacharya@gmail.com>
2024-06-29 11:56:57 +0530

    First commit

---
+++ Backend/Models/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const mongo_url = process.env.MONGO_CONN;
+
+mongoose.connect(mongo_url)
+    .then(
+        () => {
+            console.log('MongoDB Connected ...');
+        }
+    ).catch(
+        (err) => {
+            console.log("MongoDB connetion Error:" , err);
+        }
+    )
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import RefrshHandler from './RefrshHandler';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />
+  }
+
+  return (
+    <div className="App">
+      <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
+      <Routes>
+        <Route path='/' element={<Navigate to="/login" />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<PrivateRoute element={<Home />} />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
